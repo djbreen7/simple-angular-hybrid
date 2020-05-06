061467a3f3b1e3853f3749a0487fa4380ac33445
Daniel Breen <daniel.breen@royaljay.local>
2020-05-06 14:35:58 -0600

    Cleanup

--- src/ng-app/app.module.ts
+++ src/ng-app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UpgradeModule } from '@angular/upgrade/static';
 import { ExampleComponent } from './example/example.component';
 
 @NgModule({
     imports: [
-        BrowserModule,
-        UpgradeModule
+        BrowserModule
     ],
     declarations: [ExampleComponent],
     entryComponents: [ExampleComponent]
 })
 export class AppModule {
-    constructor(private upgrade: UpgradeModule) { }
+    constructor() { }
     ngDoBootstrap() {
     }
 }
